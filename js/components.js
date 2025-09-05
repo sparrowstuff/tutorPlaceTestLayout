96ddac12a8bb610f3e48523a88a0a87a6f10ac02
sparrowstuff <damnagett@gmail.com>
2025-09-05 18:56:39 +0300

    добавлен блюр на главный блок страницы
    весь контент страницы ЗА ИСКЛЮЧЕНИЕМ header внесен в блок который блюрит страницу при открытом бургер меню

--- js/components.js
+++ js/components.js
@@ -1,10 +1,19 @@
 function openBurgerMenu() {
 	const burgerBtn = document.getElementById('burgerBtn')
 	const navMobileMenu = document.getElementById('mainNavMobile')
+	const bodyBlurEl = document.querySelector('.body-blur')
+	const headerEl = document.querySelector('header')
 
 	burgerBtn.addEventListener('click', () => {
 		burgerBtn.classList.toggle('burger-btn--opened')
 		navMobileMenu.classList.toggle('main-nav--show')
+
+		if (navMobileMenu.classList.contains('main-nav--show')) {
+			bodyBlurEl.style.filter = 'blur(9px)'
+			headerEl.style.filter = 'unset'
+		} else {
+			bodyBlurEl.style.filter = 'unset'
+		}
 	})
 }
 
