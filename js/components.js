4dfee98b6a0eee4294959b52a99e024571d682d4
sparrowstuff <damnagett@gmail.com>
2025-09-06 22:58:21 +0300

    модальное окно готово на 80%
    осталось задать появление при нажатии на кнопку

--- js/components.js
+++ js/components.js
@@ -15,7 +15,37 @@ function openBurgerMenu() {
 				block.style.filter = 'unset'
 			}
 		})
+
+		window.addEventListener('resize', () => {
+			if (window.innerWidth > 1024) {
+				navMobileMenu.classList.remove('main-nav--show')
+				burgerBtn.classList.remove('burger-btn--opened')
+				sectionBlocks.forEach(block => (block.style.filter = 'unset'))
+			}
+		})
 	})
 }
 
-export { openBurgerMenu }
+function submitFormActions() {
+	const form = document.getElementById('modalForm')
+	const formInput = document.getElementById('formInput')
+	const errorText = document.getElementById('inputError')
+	const closeBtn = document.getElementById('modalCloseBtn')
+
+	form.addEventListener('submit', e => {
+		e.preventDefault()
+
+		if (formInput.value.trim() === '') {
+			formInput.classList.add('custom-form__input--error')
+			errorText.style.opacity = '1'
+		}
+	})
+
+	formInput.addEventListener('input', e => {
+		if (e.target.value.trim() !== '') {
+			errorText.style.opacity = '0'
+			formInput.classList.remove('custom-form__input-error')
+		}
+	})
+}
+export { openBurgerMenu, submitFormActions }
--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@ import * as components from './components.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 	components.openBurgerMenu()
+
+	components.submitFormActions()
 })
